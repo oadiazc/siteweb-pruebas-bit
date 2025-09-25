a95bc4fc2a0b584d90a2a10c2107743ecdcc4e7b
Oscar Andres Diaz Castaño <andres.diazcastano@gmail.com>
2025-09-24 22:33:07 -0500

    reemplazo por un nuevo proyecto este se llama replica la dian

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-alert("Hola Bienvendio a la tienda Sport Andres");
+const slide = document.querySelector('.carrusel-slide');
+const images = document.querySelectorAll('.carrusel-slide img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let index = 0;
+
+// Función para actualizar la posición
+function mostrarImagen() {
+  slide.style.transform = `translateX(${-index * 100}%)`;
+}
+
+// Botón siguiente
+nextBtn.addEventListener('click', () => {
+  index = (index + 1) % images.length;
+  mostrarImagen();
+});
+
+// Botón anterior
+prevBtn.addEventListener('click', () => {
+  index = (index - 1 + images.length) % images.length;
+  mostrarImagen();
+});
